e7421d0d0268b717e4ad28d359535a4dac2250e7
unknown <Igoor1>
2022-04-12 17:20:17 -0300

    first

---
+++ desaula/atendimento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desaula
+{
+    class atendimento
+    {
+        public string nome, cpf, sexo, prioridade;
+        atendimento[] lista = new atendimento[9];
+
+
+        public void registrar()
+        {
+            Console.WriteLine("Nome do Paciente:");
+           nome = Console.ReadLine();
+            lista[0] = nome;
+        }
+    }
+}
